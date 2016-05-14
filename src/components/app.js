cce7b9a8a7d9bce2472b86b8e4d3d948277cf4b3
Andrew Heekin <andrewheekin@gmail.com>
2016-05-14 12:55:53 -0400

    style

--- src/components/app.js
+++ src/components/app.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <Header />
         <SearchBar />
         <WeatherList />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="row footer">
+      <div className="col-md-4 left"><a href="" className="h5">About</a></div>
+      <div className="col-md-4 center"><a href="" className="h5">About</a></div>
+      <div className="col-md-4 right"><a href="" className="h5">About</a></div>
+      <hr />
+    </div>
+  );
+}
